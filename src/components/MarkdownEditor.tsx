e8ea8150d205732dd0f59f3a12ad372de665a1af
BhaveshS03 <bhaveshsonar003@gmail.com>
2025-08-15 23:19:25 +0530

    fixx

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -1,50 +1,48 @@
 import type { FC } from "react";
 import { Crepe } from "@milkdown/crepe";
 import { Milkdown, useEditor } from "@milkdown/react";
-import { collab, collabServiceCtx  } from "@milkdown/plugin-collab";
-import { Doc } from "yjs";
+import { collab, collabServiceCtx } from "@milkdown/plugin-collab";
+import { Doc, XmlFragment } from "yjs";
 import { WebsocketProvider } from "y-websocket";
 import "@milkdown/crepe/theme/common/style.css";
 import "@milkdown/crepe/theme/frame.css";
+import React, { useEffect } from "react";
 
-const markdown = `# Milkdown React Crepe
-
-> You're scared of a world where you're needed.
-
-This is a demo for using Crepe with **React**.`;
-
-const setupCollab = (crepe : Crepe) => {
+export const MarkdownEditor: FC = () => {
+  const [crepe, setCrepe] = React.useState<Crepe | null>(null);
   const doc = new Doc();
   const wsProvider = new WebsocketProvider(
     "ws://localhost:1234",
     "milkdown-demo",
     doc
   );
-  console.log("[CLIENT] Connected to WebSocket:", wsProvider.url);
-  console.log("[CLIENT] Milkdown ready, initializing collab...");
-  if(crepe.editor.status == "Created"){
-    crepe.editor.action((ctx) => {
-      const collabService = ctx.get(collabServiceCtx);
-      collabService
-        .bindDoc(doc)
-        .setAwareness(wsProvider.awareness)
-        .connect();
-
-      console.log("[CLIENT] Collab service bound to doc");
-    });
-  }
-}
-
-export const MarkdownEditor: FC = () => {
+  
   useEditor((root) => {
-    const crepe = new Crepe({
+    const editorInstance = new Crepe({
       root,
-      defaultValue: markdown,
+      defaultValue: "",
     });
-    crepe.editor.use(collab);
-    setupCollab(crepe);
-    return crepe;
+
+    editorInstance.editor.use(collab);
+    setCrepe(editorInstance);
+    return editorInstance;
   }, []);
+
+  useEffect(() => {
+    if (!crepe) return;
+
+    const setup = () => {
+      if (crepe.editor.status === "Created") {
+        crepe.editor.action((ctx) => {
+          const collabService = ctx.get(collabServiceCtx);
+          collabService.bindDoc(doc).setAwareness(wsProvider.awareness).connect();
+          console.log("[CLIENT] Collab service bound to shared doc");
+        });
+      }
+    };
+
+    setup();
+  }, [crepe]); 
 
   return <Milkdown />;
 };
