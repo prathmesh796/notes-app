910838ad499e5e8d95a2c4ca8d479d4a431d6d34
BhaveshS03 <bhaveshsonar003@gmail.com>
2025-08-15 18:30:38 +0530

    seprate yjs connector

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,54 +1,9 @@
 import { useState, useRef, useEffect } from "react";
-import { useInstance } from "@milkdown/react";
-import { editorViewCtx } from "@milkdown/kit/core";
-import { collab, collabServiceCtx } from "@milkdown/plugin-collab";
 import { MarkdownEditor } from "./MarkdownEditor";
-import { Doc } from "yjs";
-import { WebsocketProvider } from "y-websocket";
 
 export function Editor() {
   const [isEditing, setIsEditing] = useState(false);
   const editorRef = useRef<HTMLDivElement>(null);
-
-  const [isLoading, getInstance] = useInstance();
-
-  useEffect(() => {
-    const setupCollab = async () => {
-      console.log("[CLIENT] Waiting for Milkdown editor instance...");
-      const editor = await getInstance();
-  
-      if (!editor) {
-        console.warn("[CLIENT] No editor instance found");
-      }
-      else {
-      console.log("[CLIENT] Editor instance:", editor);
-  
-      const doc = new Doc();
-      const wsProvider = new WebsocketProvider(
-        "ws://localhost:1234",
-        "milkdown-demo",
-        doc
-      );
-      console.log("[CLIENT] Connected to WebSocket:", wsProvider.url);
-      console.log("[CLIENT] Milkdown ready, initializing collab...");
-      editor.action((ctx) => {
-        const collabService = ctx.get(collabServiceCtx);
-        collabService
-          .bindDoc(doc)
-          .setAwareness(wsProvider.awareness)
-          .connect();
-  
-        console.log("[CLIENT] Collab service bound to doc");
-  
-        const view = ctx.get(editorViewCtx);
-        console.log("[CLIENT] EditorView ready:", view);
-      });
-    }
-    };
-  
-    setupCollab();
-  }, [getInstance]);
-  
 
   useEffect(() => {
     const editorEl = editorRef.current;
--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -1,8 +1,9 @@
 import type { FC } from "react";
 import { Crepe } from "@milkdown/crepe";
 import { Milkdown, useEditor } from "@milkdown/react";
-import { collab } from "@milkdown/plugin-collab";
-
+import { collab,collabServiceCtx  } from "@milkdown/plugin-collab";
+import { Doc } from "yjs";
+import { WebsocketProvider } from "y-websocket";
 import "@milkdown/crepe/theme/common/style.css";
 import "@milkdown/crepe/theme/frame.css";
 
@@ -12,6 +13,26 @@ const markdown = `# Milkdown React Crepe
 
 This is a demo for using Crepe with **React**.`;
 
+const setupCollab = (crepe : Crepe) => {
+  const doc = new Doc();
+  const wsProvider = new WebsocketProvider(
+    "ws://localhost:1234",
+    "milkdown-demo",
+    doc
+  );
+  console.log("[CLIENT] Connected to WebSocket:", wsProvider.url);
+  console.log("[CLIENT] Milkdown ready, initializing collab...");
+  crepe.editor.action((ctx) => {
+    const collabService = ctx.get(collabServiceCtx);
+    collabService
+      .bindDoc(doc)
+      .setAwareness(wsProvider.awareness)
+      .connect();
+
+    console.log("[CLIENT] Collab service bound to doc");
+  });
+}
+
 export const MarkdownEditor: FC = () => {
   useEditor((root) => {
     const crepe = new Crepe({
@@ -19,6 +40,7 @@ export const MarkdownEditor: FC = () => {
       defaultValue: markdown,
     });
     crepe.editor.use(collab);
+    setupCollab(crepe);
     return crepe;
   }, []);
 
